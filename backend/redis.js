87531fa721a9d7230e31c360c233e6b9d027374a
saviruperera <saviru.perera@mail.mcgill.ca>
2026-02-12 16:49:44 +1100

    redis fix

--- backend/redis.js
+++ backend/redis.js
@@ -2,10 +2,16 @@ import IORedis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const redis = new IORedis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: undefined,
+const redisOptions = {
     maxRetriesPerRequest: null,
     enableReadyCheck: false
-});
+};
+
+export const redis = process.env.REDIS_URL
+    ? new IORedis(process.env.REDIS_URL, redisOptions)
+    : new IORedis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT || '6379'),
+        password: process.env.REDIS_PASSWORD || undefined,
+        ...redisOptions
+    });
